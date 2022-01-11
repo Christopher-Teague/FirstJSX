94ce3e0a6a457aa13cb2719350908f4cf83e3d41
Christopher-Teague <cmteague77@gmail.com>
2022-01-11 11:48:09 -0800

    first commit

--- src/App.js
+++ src/App.js
@@ -4,20 +4,14 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Hello Dojo!</h1>
+      <h3>Things I need to do:</h3>
+      <ul>
+        <li>Wake up</li>
+        <li>Make kids breakfast and lunch for school</li>
+        <li>COFFEE!!!</li>
+        <li>MERN it out!</li>
+      </ul>
     </div>
   );
 }
